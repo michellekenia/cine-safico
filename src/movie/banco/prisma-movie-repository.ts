d7197edb8b8e5cef3d95aa498d9e628f8464a3f3
mih <keniamsoaress@gmail.com>
2024-10-17 12:36:44 -0300

    criar-repositories

--- src/movie/banco/prisma-movie-repository.ts
+++ src/movie/banco/prisma-movie-repository.ts
@@ -1,32 +1,57 @@
-import { Movie } from "@prisma/client";
-import { CreateMovieDto } from "src/movie/dto/create-movie.dto";
 import { UpdateMovieDto } from "src/movie/dto/update-movie.dto";
+import { MovieRepositoryPort } from "src/core/ports/movie-repository-port";
+import { Movie } from "src/core/domain/movie.model";
 import { PrismaService } from "src/adapters/prisma.service";
+import { CreateMovieDto } from "../dto/create-movie.dto";
 
-export class PrismaMovieRepository {
+export class PrismaMovieRepository implements MovieRepositoryPort {
 
-    constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prismaService: PrismaService) { }
 
-    async create(data: CreateMovieDto): Promise<Movie> {
-        return this.prisma.movie.create({data})
+    async create(createMovieDto: CreateMovieDto): Promise<Movie> {
+        try {
+            return await this.prismaService.movie.create({ data: createMovieDto })
+        } catch (error) {
+            throw new Error(`Erro ao criar o filme: ${error.message}`)
+        }
+    }
+
+    async update(id: number, updateMovieDto: UpdateMovieDto): Promise<Movie | null> {
+        try {
+            return await this.prismaService.movie.update({
+                where: { id },
+                data: updateMovieDto
+            })
+        } catch (error) {
+            throw new Error(`Erro ao atualizar o filme: ${error.message}`);
+        }
+    }
+
+    async findById(id: number): Promise<Movie | null> {
+        try {
+            return await this.prismaService.movie.findUnique({
+                where: { id },
+            });
+        } catch (error) {
+            throw new Error(`Erro ao buscar o filme: ${error.message}`);
+        }
     }
 
     async findAll(): Promise<Movie[]> {
-        return this.prisma.movie.findMany()
+        try {
+            return await this.prismaService.movie.findMany();
+        } catch (error) {
+            throw new Error(`Erro ao buscar filmes: ${error.message}`);
+        }
     }
 
-    async update(id: number, data: UpdateMovieDto): Promise <Movie | null> {
-        return this.prisma.movie.update({
-            where: { id },
-            data
-        })
+    async delete(id: number): Promise<void> {
+        try {
+            await this.prismaService.movie.delete({
+                where: { id },
+            });
+        } catch (error) {
+            throw new Error(`Erro ao deletar o filme: ${error.message}`);
+        }
     }
-
-    async delete (id: number): Promise <Movie | null> {
-        return this.prisma.movie.delete ({
-            where: { id}
-        })
-
-    }
-
 }
