1443c5c2ef885175fac22c52978dc8739fc92782
mih <keniamsoaress@gmail.com>
2026-04-01 16:02:15 -0300

    fix: update next page navigation condition in MovieParserService for testing

--- src/scraper/services/movie-parser.service.ts
+++ src/scraper/services/movie-parser.service.ts
@@ -121,7 +121,8 @@ export class MovieParserService implements IMovieParser {
 
         // Tentar ir para próxima página
         const nextButton = await page.$(SELECTORS.list.nextPageButton);
-        if (nextButton && pageCount < 1) { 
+        //Para testes usar após nextButton ->  && pageCount < 1
+        if (nextButton) { 
           try {
             await nextButton.click();
             await page.waitForSelector(SELECTORS.list.movieFrame, { timeout: 15000 });
